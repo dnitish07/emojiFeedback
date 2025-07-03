af411ce7718d6f6d6a0fe0a0566817df2fedbe58
Nitish Doddi <dnitishvvit2022@gmail.com>
2025-07-03 11:58:53 +0530

    Updated

--- script.js
+++ script.js
@@ -33,3 +33,9 @@ stars.forEach(function (star, index) {
 
     });
 });
+
+let toggle = document.getElementById('darkModeToggle');
+
+toggle.addEventListener('change', function () {
+    document.body.classList.toggle('dark-mode', this.checked);
+});
